5ed4c600e87134fe2eeeb7b89b7a2766bcbfdb83
atleastzero <megan.obryan52@gmail.com>
2021-05-04 12:31:34 -0700

    Got name changes working

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+users_nicks = {};
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  users_nicks[socket.id] = socket.id;
+
+  socket.on('name change', (newName) => {
+    users_nicks[socket.id] = newName;
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', users_nicks[socket.id] + ": " + msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
